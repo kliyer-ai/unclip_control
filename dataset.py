e8bf91c14e84ee5e84d85d8a591223df174c03aa
kliyer-ai <mail@nickstracke.dev>
2023-05-29 14:26:20 +0200

    add check video

--- dataset.py
+++ dataset.py
@@ -5,13 +5,12 @@ import numpy as np
 from torch.utils.data import Dataset
 
 
-
 class MyDataset(Dataset):
     def __init__(self, name):
-        self.path = './data/' + name + '/'
+        self.path = "../ControlNet/data/" + name + "/"
 
         self.data = []
-        with open(self.path + 'data.json', 'rt') as f:
+        with open(self.path + "data.json", "rt") as f:
             for line in f:
                 self.data.append(json.loads(line))
 
@@ -21,10 +20,10 @@ class MyDataset(Dataset):
     def __getitem__(self, idx):
         item = self.data[idx]
 
-        source_filename = item['source']
-        hint_filename = item['hint']
-        target_filename = item['target']
-        prompt = item['prompt']
+        source_filename = item["source"]
+        hint_filename = item["hint"]
+        target_filename = item["target"]
+        prompt = item["prompt"]
 
         hint = cv2.imread(self.path + hint_filename)
         source = cv2.imread(self.path + source_filename)
@@ -56,5 +55,10 @@ class MyDataset(Dataset):
         # source = (source.astype(np.float32) / 127.5) - 1.0
 
         # style does not need to get normalized as it happens in the model later
-        return dict(jpg=target, txt="a professional, detailed, high-quality image", hint=hint, style=source, meta={'file_name':source_filename})
-
+        return dict(
+            jpg=target,
+            txt="",
+            hint=hint,
+            style=source,
+            meta={"file_name": source_filename},
+        )
