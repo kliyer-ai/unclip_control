9ad0157bfb0ca2e2f184e25933ab1e72cc36545a
kliyer-ai <mail@nickstracke.dev>
2023-07-03 10:41:01 +0200

    fix logvar problem for old checkpoints

--- tool_remove_logvar.py
+++ tool_remove_logvar.py
@@ -15,15 +15,6 @@ from share import *
 from cldm.model import create_model
 
 
-def get_node_name(name, parent_name):
-    if len(name) <= len(parent_name):
-        return False, ""
-    p = name[: len(parent_name)]
-    if p != parent_name:
-        return False, ""
-    return True, name[len(parent_name) :]
-
-
 model = create_model(config_path="./models/cldm_v15_cross.yaml")
 
 pretrained_weights = torch.load(input_path)
@@ -33,17 +24,14 @@ if "state_dict" in pretrained_weights:
 scratch_dict = model.state_dict()
 
 target_dict = {}
+
+print(scratch_dict.keys())
+
+# log var won't be in the scratch_dict
+# so it's not copied over from the pretrained weights (checkpoint)
 for k in scratch_dict.keys():
-    is_control, name = get_node_name(k, "control_")
-    if is_control:
-        copy_k = "model.diffusion_" + name
-    else:
-        copy_k = k
-    if copy_k in pretrained_weights:
-        target_dict[k] = pretrained_weights[copy_k].clone()
-    else:
-        target_dict[k] = scratch_dict[k].clone()
-        print(f"These weights are newly added: {k}")
+    target_dict[k] = pretrained_weights[k].clone()
+
 
 model.load_state_dict(target_dict, strict=True)
 torch.save(model.state_dict(), output_path)
